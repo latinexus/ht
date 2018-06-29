6e6c37e0b5414388906fdf5162c8dfe90e3e4f2a
Eric Larrea <elapez@gmail.com>
2018-06-29 15:45:26 -0500

    prueba 2

---
+++ test/SimplySlugTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Creator: Eric Larrea
+ * E-mail: eric@latinexus.net
+ * From: latinexus.net
+ * Date: 6/29/2018
+ * Time: 3:38 PM
+ */
+class SimplySlugTest extends \PHPUnit_Framework_TestCase
+{
+    public function nothingTest()
+    {
+        $this->assertTre(true);
+    }
+}
